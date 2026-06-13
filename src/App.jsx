3d7539de45484790943afe34b77553040f261590
Sivaramakrishna Yerramsetti <sivaramkrishna310@gmail.com>
2026-06-13 13:30:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import * as d3 from "d3";
+
+function App() {
+
+  return (
+    <>
+      <h1>This is my forst react App</h1>
+    </>
+  );
+}
+
+export default App;
